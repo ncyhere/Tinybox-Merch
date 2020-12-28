f54b256a5e6c4e336a52c748798f90c7d9eae193
Mykal Machon <mykalmachon@gmail.com>
2020-12-27 20:06:33 -0800

    fix: build toolchain

--- out/_next/static/K6RFtvcT7mgqfqbPgqWbN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-7250d274702238b546bd.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d2fae256d63f3a883f1c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
