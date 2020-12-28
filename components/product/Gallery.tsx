00d20a222a7329438ccc1c75aa3b956fa3cb633e
Mykal Machon <mykalmachon@gmail.com>
2020-12-28 13:26:23 -0800

    fix: add explicit image height/width

--- components/product/Gallery.tsx
+++ components/product/Gallery.tsx
@@ -8,23 +8,17 @@ type GalleryProps = {
 
 const Gallery: FunctionComponent<GalleryProps> = ({ staticProduct, state }) => {
   // initial static render...
-  if (state.loading) {
-    return (
-      <div className="p-8">
-        <img
-          src={staticProduct.image.src}
-          alt={`picture of ${staticProduct.title} in ${staticProduct.title}`}
-          className="w-full h-auto"
-        />
-      </div>
-    );
-  }
-  // render once data is loaded...
   return (
     <div className="p-8">
       <img
-        src={state.selectedVariant.image.src}
-        alt={`picture of ${state.product.title} in ${state.selectedVariant.title}`}
+        width="440px"
+        height="440px"
+        src={
+          state.loading
+            ? staticProduct.image.src
+            : state.selectedVariant.image.src
+        }
+        alt={`picture of ${staticProduct.title}`}
         className="w-full h-auto"
       />
     </div>
