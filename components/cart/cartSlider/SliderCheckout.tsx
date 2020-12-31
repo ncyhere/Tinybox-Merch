0a8c724f1c07f3dc1d112f726954a4448c35014c
Mykal Machon <mykalmachon@gmail.com>
2020-12-31 00:01:24 -0800

    fix: remove checkout when cart is empty

--- components/cart/cartSlider/SliderCheckout.tsx
+++ components/cart/cartSlider/SliderCheckout.tsx
@@ -5,14 +5,16 @@ const SliderCheckout = ({ cart, loading }) => {
         <span>Subtotal:</span>
         <span className="justify-self-end">${cart.subtotalPrice}</span>
       </p>
-      <a
-        className={` ${
-          loading && 'animate-pulse bg-opacity-50'
-        } flex items-center justify-center w-full px-4 py-2 my-4 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50`}
-        href={loading ? null : cart.webUrl}
-      >
-        Checkout
-      </a>
+      { cart.lineItems.length > 0 ? (
+        <a
+          className={` ${
+            loading && 'animate-pulse bg-opacity-50'
+          } flex items-center justify-center w-full px-4 py-2 my-4 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50`}
+          href={loading ? null : cart.webUrl}
+        >
+          Checkout
+        </a>
+      ) : null }
     </div>
   );
 };
