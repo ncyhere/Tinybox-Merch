8ffc93352f0a5b4aeeb22aad339f96b2dcdb865b
Mykal Machon <mykal@tinybox.dev>
2021-02-14 16:10:18 -0800

    feat: adding generic year

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => (
   <footer className="grid text-sm text-gray-400 h-28 place-items-center">
-    Tinybox Software Development. 2020
+    Tinybox Software Development. {new Date().getFullYear()}
   </footer>
 );
 
